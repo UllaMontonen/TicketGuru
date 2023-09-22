d946b8a5daca62d9a792be5fba0f816047032c33
UllaMontonen <u.montonen@gmail.com>
2023-09-22 18:43:46 +0300

    changing the name of the Controller file and adding comments

--- src/main/java/SKRUM/TicketGuru/web/TicketGuruController.java
+++ src/main/java/SKRUM/TicketGuru/web/TicketGuruController.java
@@ -11,48 +11,43 @@ import SKRUM.TicketGuru.domain.EventRepository;
 import SKRUM.TicketGuru.domain.Event;
 
 @Controller
-public class EventController {
+public class TicketGuruController {
 
 	@Autowired
 	private EventRepository repository;
 	
 	
-	// show all events
+	// *** EVENT CONTROLLER ***
+	// EVENT: show all events
 	@GetMapping({"/eventlist"})
 	public String eventlist(Model model) {
 		model.addAttribute("events", repository.findAll());
 		return "eventlist";
 	}
-	
-	
-	// add new event
+	// EVENT: add new event
 	@GetMapping("/eventlist/add")
 	public String addEvent(Model model) {
 		model.addAttribute("event", new Event());
 		return "addevent";
 	}
-	
-	
-	// save new event
+	// EVENT: save new event
 	@PostMapping("/eventlist/save")
 	public String save(Event event) {
 		repository.save(event);
 		return "redirect:eventlist";
 	}
-	
-	
-	// delete event
+	// EVENT: delete event
 	@GetMapping("/eventlist/delete/{id}")
 	public String deleteEvent(@PathVariable("id") long id, Model model) {
 		repository.deleteById(id);
 		return "redirect:.../eventlist";
 	}
-	
-	
-	// edit event
+	// EVEMT: edit event
 	@GetMapping("/eventlist/edit/{id}")
 	public String editEvent(@PathVariable("id") Long id, Model model) {
 		model.addAttribute("event", repository.findById(id));
 	return "editevent";
 	}
+	// *** END OF EVENT CONTROLLER ***
+	
 }
